3dfd986957d658b4017951b152f8c0a261244415
Ilia Reingold <ilia.reingold@gmail.com>
2014-04-11 17:14:44 -0400

    made some good progress

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export PATH=/usr/local/bin:#PATH
+export PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,13 +2,27 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+let(:base_title) {"Ruby on Rails Tutorial Sample App"} 
+
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_title("#{base_title}")
     end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Sample App')
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('Home') 
+
   end
+end
 
   describe "Help page" do
 
@@ -28,6 +42,13 @@ describe "Static pages" do
   it "should have the right title" do
     visit '/static_pages/home'
     expect(page).to have_title("Ruby")
+  end
+
+  describe "Contact page" do
+    it "should have a name for a new contact" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
   end
 
 end
