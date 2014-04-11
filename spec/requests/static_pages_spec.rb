95c710312f80f7845075bcf99dc1703de9af323e
Ilia Reingold <ilia.reingold@gmail.com>
2014-04-11 18:01:33 -0400

    need to fix routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,17 +8,17 @@ let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('Sample App')
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('Home') 
 
   end
@@ -27,27 +27,41 @@ end
   describe "Help page" do
 
   	it "should have the content 'Help' " do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
   	end
+
+    it "should have the title: 'Help'" do
+      visit help_path
+      expect(page).to have_content('Help')
+    end
+
   end
 
   describe "About page" do 
+
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
   end
 
   it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title("Ruby")
+    visit about_path
+    expect(page).to have_title("About")
   end
 
   describe "Contact page" do
+
     it "should have a name for a new contact" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact" do
+      visit contact_path
+      expect(page).to have_title('Contact')
+
     end
   end
 
